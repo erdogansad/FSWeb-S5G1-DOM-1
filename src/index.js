ed4af3fbf23ae11c1de01036c84f95677e345f1e
erdogansad <60552846+erdogansad@users.noreply.github.com>
2023-04-17 13:46:33 +0300

    sorular çözüldü

--- src/index.js
+++ src/index.js
@@ -43,3 +43,81 @@ console.log('Proje açıldı!')
 
 
 /* Kodlar Buradan aşağıya */
+
+
+const body = document.querySelector("body"), container = body.querySelector(".container");
+
+
+/* navigation bar düzenlemesi */
+const header = container.querySelector("header"),
+      nav = header.querySelector("nav"),
+      navItems = nav.querySelectorAll("a");
+for(let i = 0; i < navItems.length; i++) {
+  navItems[i].className = "italic";
+  navItems[i].innerHTML = siteContent["nav"]["nav-item-"+(i+1)];
+};
+
+
+/* logo düzenlemesi */
+const logo = header.querySelector("img");
+logo.src = siteContent["images"]["logo-img"];
+
+
+/* cta düzenlemesi */
+const cta =  container.querySelector(".cta"),
+      ctaText = cta.querySelector(".cta-text"),
+      ctaImg = cta.querySelector("#cta-img");
+
+ctaText.querySelector("h1").innerHTML = siteContent["cta"]["h1"];
+ctaText.querySelector("button").innerHTML = siteContent["cta"]["button"];
+ctaImg.src = siteContent["images"]["cta-img"];
+
+
+/* main-content düzenlemesi */
+const mainContent = container.querySelector(".main-content"),
+      topContent = mainContent.querySelector(".top-content"),
+      middleImg = mainContent.querySelector("img"),
+      bottomContent = mainContent.querySelector(".bottom-content");
+
+
+const topContentElements = topContent.querySelectorAll(".text-content");
+let topContentIdx = [["özellikler", "hakkımızda"], ["h4", "içerik"]]; 
+
+for(let i = 0; i < topContentElements.length; i++){
+  let title = topContentElements[i].querySelector("h4"),
+      text = topContentElements[i].querySelector("p");
+
+  title.innerHTML = siteContent["ana-içerik"][`${topContentIdx[0][i]}-${topContentIdx[1][0]}`];
+  text.innerHTML = siteContent["ana-içerik"][`${topContentIdx[0][i]}-${topContentIdx[1][1]}`];
+}
+
+
+middleImg.src = siteContent["images"]["accent-img"];
+
+
+const bottomContentElements = bottomContent.querySelectorAll(".text-content");
+let bottomContentIdx = [["servisler", "ürünler", "vizyon"], ["h4", "içeriği"]]; 
+
+for(let i = 0; i < bottomContentElements.length; i++){
+  let title = bottomContentElements[i].querySelector("h4"),
+      text = bottomContentElements[i].querySelector("p");
+
+  title.innerHTML = siteContent["ana-içerik"][`${bottomContentIdx[0][i]}-${bottomContentIdx[1][0]}`];
+  text.innerHTML = siteContent["ana-içerik"][`${bottomContentIdx[0][i]}-${bottomContentIdx[1][1]}`];
+}
+
+
+/* contact-section düzenlemesi */
+const contact = container.querySelector(".contact"),
+      contactElements = contact.children;
+let contactIdx = Object.keys(siteContent["iletisim"]);
+
+for(let i in contactIdx){
+  contactElements[i].innerHTML = siteContent["iletisim"][contactIdx[i]];
+}
+
+
+/* footer düzenlemesi */
+const footer = container.querySelector("footer");
+footer.children[0].className = "bold";
+footer.children[0].innerHTML = siteContent["footer"]["copyright"];
